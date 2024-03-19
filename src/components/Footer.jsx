defb24faabd32b13a9d57af8a653ca0be67197d0
harpreet <harpreetx01@outlook.com>
2024-03-19 03:56:15 -0700

    implemented the footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,29 +9,51 @@ function Footer() {
   const handleEmailClick = () => {
     window.location.href = `mailto:${email}`;
   };
+
   return (
     <div>
       <div className="grid grid-cols-3 items-center justify-between gap-x-8 px-12 py-12 bg-black text-white text-lg border-t border-white">
         <h1 className="font-logo text-xl  ml-8 text-blue">Harpreet Singh</h1>
         <div>
           <h2 className="text-xl mb-8">Profiles</h2>
-          <div className="flex gap-4 items-center mb-4 cursor-pointer ">
-            <FaInstagram className="text-blue " />
-            <span className=" transition duration-300 ease-in-out hover:bg-blue">
-              Intsagram
-            </span>
+          <div className="flex gap-4 items-center mb-4 cursor-pointer">
+            <a
+              href="https://www.instagram.com/harpreet1o/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex gap-1 items-center"
+            >
+              <FaInstagram className="text-blue" />
+              <span className=" transition duration-300 ease-in-out hover:bg-blue">
+                Intsagram
+              </span>
+            </a>
           </div>
           <div className="flex gap-4 items-center mb-4 cursor-pointer">
-            <FaLinkedinIn className="text-blue" />
-            <span className=" transition duration-300 ease-in-out hover:bg-blue">
-              LinkedIn
-            </span>
+            <a
+              href="www.linkedin.com/in/harpreet-singh-31a912258"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex gap-1 items-center"
+            >
+              <FaLinkedinIn className="text-blue" />
+              <span className=" transition duration-300 ease-in-out hover:bg-blue">
+                LinkedIn
+              </span>
+            </a>
           </div>
           <div className="flex gap-4 items-center mb-4 cursor-pointer">
-            <FaGithub className="text-blue" />
-            <span className=" transition duration-300 ease-in-out hover:bg-blue">
-              GitHub
-            </span>
+            <a
+              href="https://github.com/harpreet1o"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex gap-1 items-center"
+            >
+              <FaGithub className="text-blue" />
+              <span className=" transition duration-300 ease-in-out hover:bg-blue">
+                GitHub
+              </span>
+            </a>
           </div>
         </div>
         <div>
